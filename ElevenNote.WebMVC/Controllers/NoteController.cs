aac76eaf4592e2527633cb6bf9014ccced93af8f
nmalcock <nmalcock97@gmail.com>
2021-08-11 11:14:32 -0400

    Added Create Class/View and Changed Layout to see Notes

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -16,5 +16,24 @@ namespace ElevenNote.WebMVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        //Add method here VVVV
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Add code here vvvv
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
